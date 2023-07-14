8d71353384c4992d70c54567747587d0c0277331
Asushei Orisaremi <emmanuelasushei@gmail.com>
2023-07-14 12:55:31 -0700

    Task O... The real one

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * Task 0 - Positive anything is better than negative nothing
  */
@@ -16,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d ", "n is positive\n")
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d ", "n is 0\n")
+		printf("%d is zero\n", n);
 	}
 	else 
 	{
-		printf("%d ", "n is negative\n")
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
